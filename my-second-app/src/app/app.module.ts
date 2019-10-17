93e08747fabafbc163396b781838b6d1d35ff73a
Israel Mesa <mesa.israel50@gmail.com>
2019-10-16 22:26:07 -0400

    playing with components

--- my-second-app/src/app/app.module.ts
+++ my-second-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { IzzyComponent } from './izzy/izzy.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { IzzyComponent } from './izzy/izzy.component';
     AppComponent,
     ServerComponent,
     ServersComponent,
-    IzzyComponent
+    IzzyComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
