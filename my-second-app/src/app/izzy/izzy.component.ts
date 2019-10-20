5cc7d688663e1bc132f8b9fd6ce2874cf30e91bd
Israel Mesa <mesa.israel50@gmail.com>
2019-10-20 13:43:23 -0400

    html/css - overflow

--- my-second-app/src/app/izzy/izzy.component.ts
+++ my-second-app/src/app/izzy/izzy.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-izzy]',
+  selector: 'app-izzy',
   templateUrl: './izzy.component.html',
   styleUrls: ['./izzy.component.css']
 })
