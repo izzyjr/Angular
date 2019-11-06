2f15b4b57c42643a898166dd680ba24ebe216aa2
Israel Mesa <mesa.israel50@gmail.com>
2019-11-05 20:04:02 -0500

    Creating Components

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './Components/RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Components/RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Components/RecipeBook/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './Components/ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './Components/ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import {HeaderComponent} from './Components/header/header.component';
+import { RecipesComponent } from './Components/RecipeBook/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
