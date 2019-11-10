0e9496a073bb11edc622feb02322427e5755eb81
Israel Mesa <mesa.israel50@gmail.com>
2019-11-09 19:01:22 -0500

    iterating through model array

--- application/src/app/Components/RecipeBook/recipe-list/recipe-list.component.ts
+++ application/src/app/Components/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from '../recipes/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: RecipeModel[] = [
+    new RecipeModel('A Test Recipe', 'This is simply a test', 'https://www.iceorrice.com/wp-content/' +
+      'uploads/2018/01/DSC00442-2.jpg'),
+    new RecipeModel('A Test Recipe 2', 'This is simply a test 2', 'https://www.iceorrice.com/wp-content/' +
+      'uploads/2018/01/DSC00442-2.jpg')
+  ];
 
   constructor() { }
 
